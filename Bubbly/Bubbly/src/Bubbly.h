a227bd8ef3ea30186865e59cfc4e9017382a865e
BuildTools <unconfigured@null.spigotmc.org>
2020-09-15 22:09:59 -0400

    moved git into folder

--- Bubbly/Bubbly/src/Bubbly.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#include "BubblyCore/Appl.h"
--- Bubbly/Bubbly/src/BubblyCore/Core.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef BBLY_PLATFORM_WIDNOWS
-	#ifdef BBLY_BUILD_DLL
-		#define BBLY_API __declspec(dllexport)
-	#else
-		#define BBLY_API __declspec(dllimport)
-	#endif
-#endif
